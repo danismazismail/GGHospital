2a8c8aaecb54da1b42278d106625a0fd2d3ba17d
Ismail DANISMAZ <danismazismail@gmail.com>
2024-06-07 15:21:56 +0300

    Doktor,Bolum,Rol Tablolari için class'lar olusturuldu

---
+++ GGHospital.Core/Izin.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GGHospital.Entities
+{
+    public class Izin
+    {
+        [Key]
+        public int IzinID { get; set; }
+
+        public DateTime? IzinBaslangicTarihi { get; set; } = DateTime.Now.AddDays(1);
+
+        [Required]
+        public DateTime IzinBitisTarihi { get; set; }
+
+        [Required]
+        public int DoktorID { get; set; }
+
+        [ForeignKey("DoktorID")]
+        public virtual List<Doktor> Doktor { get; set; }
+    }
+}
