36b81b5ba71f1482c6de6b78020325b824887214
Ismail DANISMAZ <danismazismail@gmail.com>
2024-06-17 16:55:59 +0300

    ismail => UI

--- GGHospital.UI/Program.cs
+++ GGHospital.UI/Program.cs
@@ -23,6 +23,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=Index}/{id?}");
+    pattern: "{controller=Deneme}/{action=Index}/{id?}");
 
 app.Run();
